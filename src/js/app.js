44380351dcb5861a0b4cc31bc97b54c0e26ba646
Gabriela Hristova <gabriela.hr1406@gmail.com>
2022-12-23 23:24:46 +0200

    add

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,11 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
+  let priceElements = document.querySelectorAll('.price');
+  console.log(priceElements);
+priceElements.forEach(element => {
+  if(element.classList.contains('hot')) {
+    element.innerHTML += '🔥';
+  }
+});
 });
